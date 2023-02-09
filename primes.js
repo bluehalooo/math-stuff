e69dd5c873aa198b5bbca71d64e325e7e30f0e0f
bluehalooo <119090993+bluehalooo@users.noreply.github.com>
2023-02-09 11:21:38 -0800

    added mersenne

--- primes.js
+++ primes.js
@@ -4,12 +4,12 @@ var prime = true;
 // If number is 1, not prime nor composite.
 if (x == 1){
     console.log("This number is 1, which is neither prime nor composite.");
-    return(0);
+    return(false);
 }
 
 if (x < 1) {
     console.log("This number is not a positive number, so it is not a prime.")
-    return(0);
+    return(false);
 }
 
 else if (x>1) {
@@ -17,13 +17,21 @@ else if (x>1) {
         if (x % i == 0) {
             console.log("This number is not prime.");
             prime = false;
-            return(0);
+            return(false);
             break;
         }
     }
 }
 if (prime == true) {
     console.log("This number is prime.")
-    return (1);
+    return (true);
 }
 }
+//The following checks if the input will work as p in the equation (2^p)-1 = p, where p is a prime.
+function mersenne(input) {
+    if (prime(input)) {
+        var willitwork = ((2**input)-1);
+        return(prime(willitwork));
+    }
+}
+
