652ad14c11e4e4334955d041620c3ed83569a1c3
bluehalooo <119090993+bluehalooo@users.noreply.github.com>
2023-02-09 13:28:23 -0800

    added findb and finda

--- Pythagorean.js
+++ Pythagorean.js
@@ -3,3 +3,10 @@ function Pythagorean(a, b) {
     var c = (a**2) + (b**2)
     return (Math.sqrt(c))
 }
+
+function findb(a,c) {
+    var b = Math.sqrt((c**2) - (a**2))
+}
+function finda(b,c) {
+    var b = Math.sqrt((c**2) - (b**2))
+}
