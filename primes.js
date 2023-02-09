57b81442c1dc6f8e0cd59baa63578c8bf22a90f5
bluehalooo <119090993+bluehalooo@users.noreply.github.com>
2023-02-09 11:10:48 -0800

    made primes.js

--- primes.js
+++ primes.js
@@ -1,1 +1,29 @@
-var x = 1
+
+function prime(x){
+var prime = true;
+// If number is 1, not prime nor composite.
+if (x == 1){
+    console.log("This number is 1, which is neither prime nor composite.");
+    return(0);
+}
+
+if (x < 1) {
+    console.log("This number is not a positive number, so it is not a prime.")
+    return(0);
+}
+
+else if (x>1) {
+    for (let i = 2; i < x; i++) {
+        if (x % i == 0) {
+            console.log("This number is not prime.");
+            prime = false;
+            return(0);
+            break;
+        }
+    }
+}
+if (prime == true) {
+    console.log("This number is prime.")
+    return (1);
+}
+}
